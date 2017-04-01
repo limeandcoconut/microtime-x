77fc6e8697d0f949e95880cd758d1add10acc084
Jacob Smith <messagethesmith@gmail.com>
2017-04-01 13:15:23 -0700

    Switch dependency to microtime-nodejs to remove dependency on node-gyp

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 Returns the number of microseconds elapsed since 1 January 1970 00:00:00 UTC
 
-In browser relies on `performance.now` or it's engine specific variant if available, in Node.js [microtime](https://npmjs.org/package/microtime) by Wade Simmons is used (if supported). If none of the mentioned works then `Date.now()` result multiplied by 1000 is returned
+In browser relies on `performance.now` or it's engine specific variant if available, in Node.js [microtime-nodejs](https://www.npmjs.com/package/microtime-nodejs) by yuri0 is used (if supported). If none of the mentioned works then `Date.now()` result multiplied by 1000 is returned
 
 ## Installation
 ### NPM
 
 In your project path:
 
-	$ npm install microtime-x
+	$ npm install @limeandcoconut/microtime-x
 
 ### Browser
 
 You can easily bundle npm packages for browser with [modules-webmake](https://github.com/medikoo/modules-webmake)
 
-## Tests [![Build Status](https://travis-ci.org/medikoo/microtime-x.png)](https://travis-ci.org/medikoo/microtime-x)
+## Tests [![Build Status](https://travis-ci.org/limeandcoconut/microtime-x.png)](https://travis-ci.org/limeandcoconut/microtime-x)
 
 	$ npm test
--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-'use strict';
+'use strict'
 
-var floor = Math.floor, correction = 0;
+var floor = Math.floor,
+    correction = 0
 
-module.exports = exports = (function () {
-	var name, round, base, dateNow, nodeMicrotime, envCorrection;
+module.exports = exports = (function() {
+    var name, round, base, dateNow, microtimeNodejs, envCorrection
 
-	if ((typeof performance !== 'undefined') && performance) {
-		if (typeof performance.now === 'function') name = 'now';
-		else if (typeof performance.mozNow === 'function') name = 'mozNow';
-		else if (typeof performance.msNow === 'function') name = 'msNow';
-		else if (typeof performance.oNow === 'function') name = 'oNow';
-		else if (typeof performance.webkitNow === 'function') name = 'webkitNow';
+    if ((typeof performance !== 'undefined') && performance) {
+        if (typeof performance.now === 'function') {
+            name = 'now'
+        } else if (typeof performance.mozNow === 'function') {
+            name = 'mozNow'
+        } else if (typeof performance.msNow === 'function') {
+            name = 'msNow'
+        } else if (typeof performance.oNow === 'function') {
+            name = 'oNow'
+        } else if (typeof performance.webkitNow === 'function') {
+            name = 'webkitNow'
+        }
 
-		if (name) {
-			round = Math.round;
-			if (performance.timing &&
+        if (name) {
+            round = Math.round
+            if (performance.timing &&
 					(typeof performance.timing.navigationStart === 'number')) {
-				base = performance.timing.navigationStart;
-			} else {
-				base = Date.now();
-			}
+                base = performance.timing.navigationStart
+            } else {
+                base = Date.now()
+            }
 			// We need to periodically calculate correction, as
 			// `performance.now()` doesn't react to eventual clock corrections
 			// (as made by system).
@@ -27,28 +34,36 @@ module.exports = exports = (function () {
 			// which may in result be rejected by other processes (as malicious).
 			// Reevaluating that correction doesn't remove that risk completely,
 			// still minimizes it's probablity
-			envCorrection = -((base + performance[name]()) - Date.now());
-			setInterval(function () {
-				envCorrection = -((base + performance[name]()) - Date.now());
-			}, 1000);
-			return function () {
-				return round((base + performance[name]() + envCorrection) * 1000) + correction;
-			};
-		}
-	}
+            envCorrection = -((base + performance[name]()) - Date.now())
+            setInterval(function() {
+                envCorrection = -((base + performance[name]()) - Date.now())
+            }, 1000)
+            return function() {
+                return round((base + performance[name]() + envCorrection) * 1000) + correction
+            }
+        }
+    }
 
-	if ((typeof process === 'object') && process) {
-		try { nodeMicrotime = (require)('microtime'); } catch (ignore) {} //jslint: ignore
-		if (nodeMicrotime) {
-			return function () { return nodeMicrotime.now() + correction; };
-		}
-	}
+    if ((typeof process === 'object') && process) {
+        try {
+            microtimeNodejs = (require)('microtime-nodejs')
+        } catch (ignore) {} // jslint: ignore
+        if (microtimeNodejs) {
+            return function() {
+                return microtimeNodejs.now() + correction
+            }
+        }
+    }
 
-	dateNow = Date.now;
-	return function () { return dateNow() * 1000 + correction; };
-}());
+    dateNow = Date.now
+    return function() {
+        return dateNow() * 1000 + correction
+    }
+})()
 
-exports.correct = function (num) {
-	if (!isFinite(num)) throw new TypeError(num + " is not valid correction value");
-	correction = floor(Number(num));
-};
+exports.correct = function(num) {
+    if (!isFinite(num)) {
+        throw new TypeError(num + ' is not valid correction value')
+    }
+    correction = floor(Number(num))
+}
